c7c110aa933ef10ae2ebce8910df77159c03d757
José Morales <jsmsalt@gmail.com>
2021-02-20 21:25:26 -0300

    Pruebas

--- src/worker.ts
+++ src/worker.ts
@@ -82,7 +82,7 @@ const client = Binance({ apiKey: config.apiKey, apiSecret: config.apiSecret });
         let emaLast20Percent = (emaLast20[emaLast20.length - 1] * 100) / emaLast20[0] - 100;
         let emaLast10Percent = (emaLast10[emaLast10.length - 1] * 100) / emaLast10[0] - 100;
 
-        return emaLast20Percent > 2 && emaLast10Percent > 1;
+        return emaLast20Percent > 1.5 && emaLast10Percent > 0.8;
     };
 
     /*
